6e0eeee01d1ae9f86960254c236e57b9c4c68d46
PDMaduranga <97090612+PDMaduranga@users.noreply.github.com>
2024-08-09 13:34:24 +0530

    Update index.js

--- Book Store- mern Stack/backend/index.js
+++ Book Store- mern Stack/backend/index.js
@@ -24,7 +24,7 @@ app.use(cors());
 
 app.get('/', (request, response)=>{
     console.log(request);
-    return response.status(234).send('Welcome to mearn stack tutorial');
+    return response.status(234).send('Welcome');
 } );
 
 app.use('/books', booksRoute);
